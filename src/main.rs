dcf38ee5c2c57204dcd7bd85ae64049dae0e5949
yairama <yairoriginal@gmail.com>
2023-10-06 22:31:55 -0500

    adding files

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use bevy::{math::*, prelude::*};
+
+const PADDLE_START_Y: f32 = 0.0;
+const PADDLE_SIZE: Vec2 = Vec2::new(120.0, 20.0);
+const PADDLE_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
+const PADDLE_SPEED: f32 = 500.0;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9)))
+        .add_systems(Update, bevy::window::close_on_esc)
+        .add_systems(Startup, setup)
+        .add_systems(FixedUpdate, move_paddle)
+        .run();
+}
+
+#[derive(Component)]
+struct Paddle;
+
+fn setup(mut commands: Commands) {
+    // camera
+    commands.spawn(Camera2dBundle::default());
+
+    // paddle
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: vec3(0., PADDLE_START_Y, 0.0),
+                ..default()
+            },
+            sprite: Sprite {
+                color: PADDLE_COLOR,
+                custom_size: Some(PADDLE_SIZE),
+                ..default()
+            },
+            ..default()
+        },
+        Paddle,
+    ));
+}
+
+fn move_paddle(input: Res<Input<KeyCode>>, time_step: Res<FixedTime>) {}
